e5932d5a62f86b22531d00d229fb06fdd5c991f2
Teresita Guzman Nader <TeresitaCGNader@users.noreply.github.com>
2020-10-13 21:58:01 -0700

    Upload of Website Plan
    Upload of E-Commerce Website Plan

---
+++ plan/index.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+<head>
+     <?php  
+        $projectname="MangaMania";
+        $description="Plan for MangaMania Website Project";
+        $sitepath="http://people.oregonstate.edu/~guzmannt/plan/";
+        $author="Teresita C. G. Nader";
+        $slogan="Never miss a chapter with MangaMania";
+      ?>
+  <title><?php echo $projectname.": ".$description.". ".$author; ?></title>
+        <meta charset="utf-8"/>
+        <meta name="description" content="<?php echo $description; ?>" />
+        <meta name="author" content="<?php echo $author; ?>" />
+        <meta name="keywords" content="Teresita Guzman Nader, anime list, keep track of your animes, Manga list, Popular Mangas, Keep your mangas in a list, Find new manga chapters, Find manga, Manga database" />
+        <meta name="robots" content="noindex, nofollow, noarchive" /> 
+
+    <!-- links -->
+        <link href="plan.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Kurale" rel="stylesheet">
+</head>
+  
+<body>
+<main>
+  <header>
+        <h1><?php echo $projectname. ": ".$description; ?> </h1>
+        <p><?php echo $slogan; ?> </p>
+        <p>by <?php echo $author; ?></p> 
+  </header>
+    <nav id="articles">
+      <ul>
+        <li><a href="#about">About</a></li> 
+        <li><a href="#audience">Audience</a></li>
+        <li><a href="#search-results">Search Results</a></li>
+        <li><a href="#promotion">Promotion</a></li> 
+        <li><a href="#analytics">Analytics</a></li>
+        <li><a href="#technical">Technical</a></li>
+        <li><a href="#design">Design</a></li>
+      </ul>
+  </nav>
+    <article id="about">
+        <h2>About this project</h2> 
+        <p>The website project MangaMania will allow the user to create an account to keep record of the mangas and animes they are currently seen, so they do
+      not lose track of the chapter or episode they are in. It will also notify the user when a new chapter or episode of an ongoing manga or anime is released. The website will also allow the user to rate each manga and anime when they are done reading it.
+      In the website, there will be a database with the information of each manga that will allow users to find new mangas to read by name or gender. The database will direct users to external websites to read the manga.
+      The website project MangaMania will not host the mangas or animes, it will be used like a link to mangas and animes to keep record of mangas and animes that the user is interested in. And the rate each user gives to a manga and anime will help the community of the 
+      website to choose a manga or anime. No option for comments will be allowed to avoid spam, and spoilers.</p>
+    </article>
+    <article id="audience">
+        <h2>Audience Demographics</h2> 
+        <p> This section describes research findings.</p>
+        <table id="sort" class="sort">
+       <caption>Target Audience Demographic Stadistics</caption> 
+        <thead>
+          <tr>
+            <th>Users</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Anime fans</th>
+            <td colspan="1">
+               According to stadistics, american anime fans tend to be teenagers and young adults aged 18 and 24
+            with a considerable number between 13 and 18. The fans are equally divided between males and females.
+            Anima has become so popular in the U.S. that Anime Expos attract many young fans. The 15th annual Anime Expo attracted 55,000 fans (<a href="http://animecons.com/events/">animecons.com</a>), up from its first
+            year in 1992. (<a href="http://factsanddetails.com/japan/cat20/sub135/item1862.html">factsanddetails.com</a>)</td>
+          </tr>
+          <tr>
+            <th>Manga fans</th>
+            <td colspan="1">
+              Manga is the cartoon version of the anime. According to stadistics, manga sales in the U.S. reached the $200 million in 2006,
+              compared to $60 million in 2002, which suggest a huge increase of U.S. manga fans. Manga become very popular among young girls, approximately 60 percent of manga 
+              readers are female. Magazines like CosmoGirl feature works by manga artists. While young male Americans increased their consumption of manga due to the video games. (<a href="http://factsanddetails.com/japan/cat20/sub135/item1862.html">http://factsanddetails.com</a>)
+            </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th colspan="4"></th>
+          </tr>
+        </tfoot>
+      </table>
+    </article>
+    <article id="search-results">
+       <h2>Search Results</h2>
+       <p> The lists below is a list of websites that offer similar services like MangaMania.</p>
+       <dl>
+         <dt><a  href="http://www.mangareader.net/"  target="_blank"> MangaReader </a></dt>
+         <dd>The website has a capability to view mangas, but it lacks the capability to record where you left. I use this website and it is very annoying that
+         I cannot save where I left a manga, and there are no updates when a new manga chapter is released.</dd>
+      </dl>
+      <dl>
+        <dt><a  href="https://myanimelist.net/"  target="_blank"> MyAnimeList</a></dt>
+        <dd>In this website the user can add their anime and mangas to a list in order to record the mangas and animes that the user has seen, but the site has anime
+        interface that is noy very user friendly. It looks confusing.</dd>
+      </dl>
+      <dl>
+        <dt><a  href="http://jkanime.net/"  target="_blank"> JK Anime</a></dt>
+        <dd>The colors of this website make it look attractive to the user, and the interface is user friendly, but the main problem with this website is the ad traffic. There are too many ads in the website. Once you get to a page, many
+        ads pop-up in a aggressive way.</dd>
+      </dl>
+      <h3>Search Phrases</h3>
+       <p> This is a list of words that will help my website to be found by users.</p>
+       <p>Read manga online, record my manga list, record my animes, make a list of my animes, make a list of my mangas, keep track of my animes, keep track of my mangas, manga and anime recorder,
+      manga and anime list, search for anime and manga online, share and rate anime and manga, the best manga and anime of 2017,
+      recommendations of anime and manga, top 10 best mangas an animes, most popular mangas and animes, rate anime and manga online, make your list of manga,
+      how to make my list of anime and manga, new chapters of manga, new release of anime episodes, search manga by gender, search anime by gender, advance search to find anime.</p>
+    </article>
+    <article id="promotion">
+       <h2>Promotion</h2>
+     <p> List of promotional ideas.</p>
+      <ul>
+        <li>Build social media groups to share the website</li>
+        <li>Allow users to like my website in Facebook to get updates</li>
+        <li>Promote my website in blogs for anime, manga and otome games</li>
+        <li>QR code that directs to my page</li>
+        <li>Mailing list</li>
+        <li>Link website with external websites for manga and anime</li>
+        <li>Promote in anime clubs</li>
+        <li>Promote in Anime expo convention</li>
+        <li>Share with classmates, friends and family</li>
+        <li>Promote with YouTube tutorials</li>
+      </ul> 
+    </article>
+    <article id="analytics">
+        <h2>Determine Success</h2>
+       <p> Ideas for success.</p>
+       <ol>
+        <li>Do surveys to determinate the market need for my website and its functionality.</li>
+        <li>If people show an interest begging to share the website.</li>
+        <li>Send out mails, and post in social media to increase web awareness of the existence of my web page.</li>
+        <li>Analyze number of visitors using google analytics.</li>
+        <li>If there is an increase in followers continue with the same tactic, if there is not a increase in visits then the strategic has to be reviewed and modified.</li>
+      </ol>  
+  </article>
+    <article id="technical">
+       <h2>Technical Details</h2>
+       <h3>Domain Names</h3>
+      <p> The list of domains that could be used for the website.</p>
+       <dl>
+         <dt>mangamania.ninja (The address does not exist, but it should look like this.)</dt>
+         <dd><a href="https://domains.google.com/registrar?s=MangaMania&hl=en&_ga=2.134536945.1212609739.1510322506-752803017.1510322506&_gac=1.86855786.1510322911.Cj0KCQiAuZXQBRDKARIsAMwpUeTYA1rN7aQ5XgPAt0YTDkbM2bOPkzpsLKlLPhQ9E0FU2jzRcaK9kA0aAqc9EALw_wcB">Google Domains</a>: Available for $19/year</dd>
+         <dt>mangamania.info (The address does not exist, but it should look like this.)</dt>
+         <dd><a href="https://domains.google.com/registrar?s=MangaMania&hl=en&_ga=2.134536945.1212609739.1510322506-752803017.1510322506&_gac=1.86855786.1510322911.Cj0KCQiAuZXQBRDKARIsAMwpUeTYA1rN7aQ5XgPAt0YTDkbM2bOPkzpsLKlLPhQ9E0FU2jzRcaK9kA0aAqc9EALw_wcB">Google Domains</a>: Available for $12/year</dd>
+         <dt>https://vimamoris.wixsite.com/mangamania (The address does not exist, but it should look like this.)</dt>
+         <dd><a href="https://www.wix.com/my-account/sites/">Vix</a>: Available for free. (Without owning domain. Vix owns the domain.)</dd>
+         <dt>https://mangamania.wordpress.com (The address does not exist, but it should look like this.)</dt>
+         <dd><a href="https://wordpress.com/">WordPress</a>: MangaMania is not available, but variations of it are available for free. (Without owning domain. WordPressordpress owns the domain.)</dd>
+      </dl>
+       <h3>Hosting</h3> 
+      <p>The first two domains are Google's domains so, if I wanted to do a profit website I would choose to use a Google domain and hosting service but because this website will be non-profit then I will not use Google.</p>
+      <p>I choose to use both Vix and Wordpress, and figure out in the way which one is more easy and efficient to use.</p>
+    </article>
+    <article id="design">
+       <h2>Design</h2>
+<figure class="alignright" >
+          <img src="img/design.jpg" alt="2-column layout" title="Design" />
+         </figure>
+       <p>
+         I am planning to do a greenish obscure background, with a aqua green color in the nav bars of the website, with either white or black letters.</p>
+       <h3> Color Scheme</h3>
+     <p>Here is a sample of the colors I could use for my
+       <a href="https://color.adobe.com/create/color-wheel/?base=2&rule=Monochromatic&selected=4&name=My%20Color%20Theme&mode=rgb&rgbvalues=0.1854781895106663,0.5,0.2924878213581108,0.6709563790213325,1,0.7829067607385691,0.3709563790213326,1,0.5849756427162216,0.33547818951066627,0.5,0.39145338036928456,0.2967651032170661,0.8,0.4679805141729773&swatchOrder=0,1,2,3,4"  target="_blank" >color scheme.</a></p>
+       <h3>Navigation</h3>
+       <p> List of navigation names planned on being used.</p>
+      <ul>
+        <li>Home</li>
+        <li>Advance search</li>
+        <li>Anime and manga List</li>
+     </ul>
+    <br class="clear" />
+    </article>
+      <footer>
+        <p>I approve of this plan and authorize <?php echo $author; ?> to begin working.</p>
+        <p class="alignright" style="border-top:1px solid black; width:30%; margin-left:5%;">Date</p>
+        <br class="clear" />
+        <p style="border-top:1px solid black; width:65%;">Signature</p>
+        <p>&copy; 2017 <?php echo $author; ?></p> 
+      </footer>
+    </main>
+  </body>
+</html>
